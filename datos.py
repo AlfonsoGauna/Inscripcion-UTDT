3c99c3f704c142845ab16fac314050ab7ffee45f
AlfonsoGauna <55064230+AlfonsoGauna@users.noreply.github.com>
2020-02-18 17:51:59 -0300

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,51 @@
+""" 
+	los datos de acceso deben ser strings 
+
+	Ejemplo:
+		usuario = '45123789'
+		clave = '010101789'
+"""
+
+usuario = ''
+clave = ''
+
+
+""" 
+	los datos horarios deben ser cargados como numeros y segun formato de
+	24 horas 
+
+	Ejemplo:
+		anio = 2020 
+		mes = 2 
+		dia = 19
+		hora = 15
+		minutos = 30
+"""
+
+anio = 2020
+mes = 1
+dia = 1
+hora = 12
+minutos = 54
+
+
+"""
+	las materias deben ser cargadas con su respectiva comision dentro de una 
+	tupple, como se ve en el ejemplo.
+	
+	ACLARACIÓN: el nombre de las materia debe estar escrito igual que en 
+				sigedu.
+
+		materias = [('Expresión Oral y Escrita','2'),  
+					('Comercio Internacional','1'),
+					('Economía Matemática II','1'), 
+					('Historia del Pensamiento Económico','2'), 
+					('Econometría','1')]
+"""	
+
+
+materias = [('',''),
+			('',''),
+			('',''),
+			('','')]
+
---
+++ inscripcion.py
@@ -0,0 +1,132 @@
+from selenium import webdriver
+from datetime import datetime
+import datos
+
+
+### PATH DEL DRIVER DE GOOGLE
+chormedriver= \
+'./chromedriver'
+
+
+### URL DE SIGEDU
+url = 'https://sigedu.utdt.edu/WebJSP/principal/login.jsp'
+
+
+### DATOS DE LA INSCRIPCION
+
+user = datos.usuario
+contra = datos.clave
+
+hora_de_inscripcion = datetime(datos.anio, datos.mes, datos.dia, datos.hora, datos.minutos)
+
+materias = datos.materias
+
+
+### LOG IN EN SIGEDU
+driver = webdriver.Chrome(chormedriver)
+driver.get(url)
+driver.find_element_by_xpath('\
+	/html/body/table[2]/tbody/tr[2]/td/form/table/tbody/tr[1]/td[3]/input[2]'\
+	).send_keys(user)
+driver.find_element_by_xpath('\
+	/html/body/table[2]/tbody/tr[2]/td/form/table/tbody/tr[2]/td[3]/input'\
+	).send_keys(contra)
+driver.find_element_by_xpath('\
+	/html/body/table[2]/tbody/tr[2]/td/form/table/tbody/tr[3]/td/input[1]'\
+	).click()
+driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[1]/div[2]/table\
+	/tbody/tr/td/table[1]/tbody/tr/td/a').click()
+
+SIGEDU = driver.current_window_handle
+
+
+### SELECCIONAR MATERIA
+driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[2]/form/\
+	table[1]/tbody/tr[2]/td/table[3]/tbody/tr[1]/td[2]/a').click()
+
+DATOS = driver.window_handles[1]
+driver.switch_to.window(DATOS)
+
+driver.find_element_by_xpath('/html/body/form/input[8]'\
+	).send_keys(materias[0][0])
+driver.find_element_by_xpath('/html/body/form/input[9]').click()
+driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td/table/\
+	tbody/tr[1]/td/a').click()
+
+driver.switch_to.window(SIGEDU)
+
+
+### SELECCIONAR COMISION
+driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[2]/form/\
+	table[1]/tbody/tr[2]/td/table[3]/tbody/tr[2]/td[2]/a').click()
+
+DATOS = driver.window_handles[1]
+driver.switch_to.window(DATOS)
+
+driver.switch_to.window(DATOS)
+
+driver.find_element_by_xpath('/html/body/form/input[8]'\
+	).send_keys(materias[0][1])
+driver.find_element_by_xpath('/html/body/form/input[9]').click()
+driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td/table/\
+	tbody/tr/td/a').click()
+
+driver.switch_to.window(SIGEDU)
+
+driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[2]/form/\
+	table[2]/tbody/tr/td/input[1]').click()
+
+
+### CONDICION DEL HORARIO PARA LA INSCRIPCION E INSCRIPCION
+while hora_de_inscripcion > datetime.now():
+	pass
+else:
+	""" inscripcion a la materia """
+	driver.refresh()
+	driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[2]/form/\
+		table[3]/tbody/tr[2]/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/a'\
+		).click()
+	confirmation = driver.switch_to_alert()
+	confirmation.accept()
+
+for i in range(len(materias)-1):
+	### SELECCIONAR MATERIA
+	driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[2]/form/\
+		table[1]/tbody/tr[2]/td/table[3]/tbody/tr[1]/td[2]/a').click()
+
+	DATOS = driver.window_handles[1]
+	driver.switch_to.window(DATOS)
+
+	driver.find_element_by_xpath('/html/body/form/input[8]'\
+		).send_keys(materias[i+1][0])
+	driver.find_element_by_xpath('/html/body/form/input[9]').click()
+	driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td/table/\
+		tbody/tr[1]/td/a').click()
+
+	driver.switch_to.window(SIGEDU)
+
+
+	### SELECCIONAR COMISION
+	driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[2]/form/\
+		table[1]/tbody/tr[2]/td/table[3]/tbody/tr[2]/td[2]/a').click()
+
+	DATOS = driver.window_handles[1]
+	driver.switch_to.window(DATOS)
+
+	driver.switch_to.window(DATOS)
+
+	driver.find_element_by_xpath('/html/body/form/input[8]'\
+		).send_keys(materias[i+1][1])
+	driver.find_element_by_xpath('/html/body/form/input[9]').click()
+	driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td/table/\
+		tbody/tr/td/a').click()
+
+	driver.switch_to.window(SIGEDU)
+
+	driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[2]/form/\
+		table[2]/tbody/tr/td/input[1]').click()
+	driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[2]/form/\
+		table[3]/tbody/tr[2]/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/a'\
+		).click()
+	confirmation = driver.switch_to_alert()
+	confirmation.accept()
